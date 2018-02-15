714f7e012336c75b131a290727a13a3641ebd6f6
Piyaviraj Ranasinghe <piyaviraj.ran@gmail.com>
2018-02-15 21:47:21 +0530

    Fix check-styles warnings x2

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,12 +13,13 @@ public class Greeter {
   }
 
   /**
- * abc
- * @param  url  an absolute URL giving the base location of the image
- * @return      the image at the specified URL
- */
+   * abc
+   * @param  someone  an absolute URL giving the base location of the image
+   * @return      the image at the specified URL
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
-  
+
 }
+
